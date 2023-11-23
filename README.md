e6b7050b7fa148c57e21cead8b6f534e46a6986f
Divya <sengardivya1503@gmail.com>
2023-11-23 11:18:54 +0530

    dads

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Todo List App
 ![image](https://github.com/sr-divya/todo-app/assets/126344404/6072566c-eb2b-484a-9f85-6ec34758f9e8)
 
 ![image](https://github.com/sr-divya/todo-app/assets/126344404/3d3814ca-eb70-4b18-a4e8-fa50e512a0aa)
+
+![Alt text](image.png)
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function selectData() {
         newEle.innerHTML = `
             <div class="task border-bottom mb-2">
                 <li>
-                    <div class="form-check" id="form">
+                    <div class="form-check d-flex" id="form">
                         <input  ${arr[k].isComplete?`checked`:''} type="checkbox" class="form-check-input" onclick="save(${arr[k].id})">
                         <label class="${arr[k].isComplete?`form-check-label`:''}">${arr[k].todo}</label>
                         <div class="icon">
